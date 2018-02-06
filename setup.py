c3e3b092ce484f3429353eb2fe86f43fa117960b
Sebastian Frelle Koch <sebastian.frelle@gmail.com>
2018-02-06 17:19:01 +0100

    removed redundant setup parameters

--- setup.py
+++ setup.py
@@ -4,9 +4,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='dtu_ml_data_mining',
-    version='0.1.0',
     description="DTU course no. 02450 project source code",
-    author="Sebastian Frelle Koch",
+    author='Sebastian Frelle Koch',
     author_email='sebastian.frelle@gmail.com',
     url='https://github.com/sebastianfrelle/dtu_ml_data_mining',
     packages=find_packages(include=['dtu_ml_data_mining']),
@@ -16,19 +15,5 @@ setup(
         'matplotlib',
         'scipy',
         'xlrd',
-    ],
-    zip_safe=False,
-    keywords='dtu_ml_data_mining',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
 )
