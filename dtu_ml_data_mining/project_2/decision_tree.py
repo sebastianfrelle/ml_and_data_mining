b4cab4c318331844185dbcfea380bba831d3cde6
Sebastian Frelle Koch <sebastian.frelle@gmail.com>
2018-04-04 17:21:58 +0200

    decision tree 2-layer cross-validation finalized for now

--- dtu_ml_data_mining/project_2/decision_tree.py
+++ dtu_ml_data_mining/project_2/decision_tree.py
@@ -1,82 +1,150 @@
 # exercise 6.1.1
-
-from matplotlib.pylab import figure, plot, xlabel, ylabel, legend, show
+import graphviz
+import numpy as np
+from matplotlib.pylab import figure, legend, plot, show, xlabel, ylabel
 from scipy.io import loadmat
 from sklearn import model_selection, tree
-import numpy as np
 
 from dtu_ml_data_mining.project_2.project_2 import *
 
-import graphviz
 
-# Tree complexity parameter - constraint on maximum depth
-tc = np.arange(2, 21, 1)
+## Cross validation ##
 
+# Outer layer. Compute 5 different optimal models and their test errors
+K_outer = 5
+K_inner = 10
+OCV = model_selection.KFold(n_splits=K_outer, shuffle=True)
+ICV = model_selection.KFold(n_splits=K_inner, shuffle=True)
 
-# Encode grades into classes for pass/fail
-y = grades[:, 2]
-for i in range(y.shape[0]):  # iterate using no. of columns in grades
-    e = y[i, :]
-    if e < 10:
-        y[i, :] = 0
-    else:
-        y[i, :] = 1
+# Error_train = np.empty((K, 1))
+# Error_test = np.empty((K, 1))
 
-print(y.shape)
-print(X_k.shape)
+gen_errors = np.zeros((19,))
+test_errors = np.zeros((K_outer,))
 
-y = y.A.ravel()
-print(y.shape)
+outer_iteration = 0
+for fold_no, (par_index, test_index) in enumerate(OCV.split(X_k), 1):
+    print(f'Outer {fold_no}/{K_outer}')
+    # extract training and test set for current OCV fold
+    X_par = X_k[par_index, :]
+    y_par = y[par_index]
+    X_test = X_k[test_index, :]
+    y_test = y[test_index]
 
-# Simple holdout-set crossvalidation
-test_proportion = 0.5
-X_train, X_test, y_train, y_test = model_selection.train_test_split(
-    X_k, y, test_size=test_proportion)
+    test_size_ratio = y_test.shape[0] / X_k.shape[0]
 
-# Initialize variables
-Error_train = np.empty((len(tc), 1))
-Error_test = np.empty((len(tc), 1))
+    for fold_no, (train_index, val_index) in enumerate(ICV.split(X_par), 1):
+        print(f'\tInner {fold_no}/{K_inner}')
 
-for i, t in enumerate(tc):
-    # Fit decision tree classifier, Gini split criterion, different pruning levels
-    dtc = tree.DecisionTreeClassifier(criterion='gini', max_depth=t)
-    dtc = dtc.fit(X_train, y_train)
+        X_train = X_par[train_index, :]
+        y_train = y_par[train_index]
+        X_val = X_par[val_index, :]
+        y_val = y_par[val_index]
 
-    # Evaluate classifier's misclassification rate over train/test data
+        val_size_ratio = y_val.shape[0] / y_par.shape[0]
+
+        k = range(2, 21) # tree complexity
+
+        # Train each model
+        for i, d in enumerate(k):
+            # print(f'\t\tTraining model no. {i + 1} out of {len(k)} (depth: {d})')
+            dtc = tree.DecisionTreeClassifier(criterion='gini', max_depth=d)
+            dtc = dtc.fit(X_train, y_train)
+            y_est_val = dtc.predict(X_val)
+
+            misclass_rate_val = sum(np.abs(y_est_val - y_val)) / float(len(y_est_val)) # validation error?
+            
+            # Add to cumulated generalization error for this particular model, 
+            # i.e. this particular choice for max depth
+            gen_errors[i] += misclass_rate_val * val_size_ratio
+    
+    # Find optimal model and train on training data
+    optimal_model_idx = np.argmin(gen_errors)
+    optimal_depth = range(2, 21)[optimal_model_idx]
+    dtc = tree.DecisionTreeClassifier(criterion='gini', max_depth=optimal_depth)
+    dtc = dtc.fit(X_par, y_par)
+    
+    # Validate against test data to obtain the test error
     y_est_test = dtc.predict(X_test)
-    y_est_train = dtc.predict(X_train)
-    misclass_rate_test = sum(np.abs(y_est_test - y_test)
-                             ) / float(len(y_est_test))
-    misclass_rate_train = sum(
-        np.abs(y_est_train - y_train)) / float(len(y_est_train))
-    Error_test[i], Error_train[i] = misclass_rate_test, misclass_rate_train
+    misclass_rate_test = sum(np.abs(y_est_test - y_test)) / float(len(y_est_test))
 
-f = figure()
-plot(tc, Error_train)
-plot(tc, Error_test)
-xlabel('Model complexity (max tree depth)')
-ylabel('Error (misclassification rate)')
-legend(['Error_train', 'Error_test'])
+    test_errors[outer_iteration] = misclass_rate_test * test_size_ratio
+    outer_iteration += 1
 
-show()
+# Use the newly calculated test errors to find the generalization error
+print(f'Test errors (weighted with respect to size of test set): {test_errors}')
+gen_error = np.sum(test_errors)
+print(f'Generalization error: {gen_error}')
 
-tc = np.arange(2, 21, 1)
-t = Error_test[0]
-k = 2
-for i, j in enumerate(tc):
-    v = Error_test[i]
-    if v < t:
-        t = v
-        k = j
+#         y_est_val = dtc.predict(X_val)
 
-print(f'lowest error for depth {k}')
+#         misclass_rate_val = sum(np.abs(y_est_val - y_val)
+#                                 ) / float(len(y_est_val))
 
-dtc = tree.DecisionTreeClassifier(criterion='gini', max_depth=k)
-dtc = dtc.fit(X_k, y)
+#         # y_est_test = dtc.predict(X_test)
+#         # y_est_train = dtc.predict(X_train)
+#         # misclass_rate_test = sum(np.abs(y_est_test - y_test)) / float(len(y_est_test))
+#         # misclass_rate_train = sum(np.abs(y_est_train - y_train)) / float(len(y_est_train))
 
-out = tree.export_graphviz(
-    dtc, out_file='tree_project_2.gvz', feature_names=k_encoded_attr_names)
-# graphviz.render('dot','png','tree_gini',quiet=False)
-src = graphviz.Source.from_file('tree_project_2.gvz')
+#         Error_test[i], Error_train[i] = misclass_rate_test, misclass_rate_train
 
-print('Ran Exercise 6.1.1')
+#     # for i, t in enumerate(tc):
+#     #     X_train = X_par[train_index, :]
+#     #     y_train = y_par[train_index]
+#     #     X_val = X_test[val_index, :]
+#     #     y_val = y_test[val_index]
+
+
+# # Tree complexity parameter - constraint on maximum depth
+# tc = np.arange(2, 21, 1)
+
+# # Simple holdout-set crossvalidation
+# test_proportion = 0.5
+# X_train, X_test, y_train, y_test = model_selection.train_test_split(
+#     X_k, y, test_size=test_proportion)
+
+# # Initialize variables
+# Error_train = np.empty((len(tc), 1))
+# Error_test = np.empty((len(tc), 1))
+
+# for i, t in enumerate(tc):
+#     # Fit decision tree classifier, Gini split criterion, different pruning levels
+#     dtc = tree.DecisionTreeClassifier(criterion='gini', max_depth=t)
+#     dtc = dtc.fit(X_train, y_train)
+
+#     # Evaluate classifier's misclassification rate over train/test data
+#     y_est_test = dtc.predict(X_test)
+#     y_est_train = dtc.predict(X_train)
+#     misclass_rate_test = sum(np.abs(y_est_test - y_test)
+#                              ) / float(len(y_est_test))
+#     misclass_rate_train = sum(
+#         np.abs(y_est_train - y_train)) / float(len(y_est_train))
+#     Error_test[i], Error_train[i] = misclass_rate_test, misclass_rate_train
+
+# f = figure()
+# plot(tc, Error_train)
+# plot(tc, Error_test)
+# xlabel('Model complexity (max tree depth)')
+# ylabel('Error (misclassification rate)')
+# legend(['Error_train', 'Error_test'])
+
+# show()
+
+# tc = np.arange(2, 21, 1)
+# t = Error_test[0]
+# k = 2
+# for i, j in enumerate(tc):
+#     v = Error_test[i]
+#     if v < t:
+#         t = v
+#         k = j
+
+# print(f'lowest error for depth {k}')
+
+# dtc = tree.DecisionTreeClassifier(criterion='gini', max_depth=k)
+# dtc = dtc.fit(X_k, y)
+
+# out = tree.export_graphviz(
+#     dtc, out_file='tree_project_2.gvz', feature_names=k_encoded_attr_names)
+# # graphviz.render('dot','png','tree_gini',quiet=False)
+# src = graphviz.Source.from_file('tree_project_2.gvz')
