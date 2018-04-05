679e14278adfbc298e792b950c2e7584df654ae3
Emilie Dahl <s153762@student.dtu.dk>
2018-04-05 11:54:40 +0200

    Not working?

--- dtu_ml_data_mining/project_2/KNN.py
+++ dtu_ml_data_mining/project_2/KNN.py
@@ -1,27 +1,36 @@
-
+from matplotlib.pyplot import (figure, hold, plot, title, xlabel, ylabel,
+                               colorbar, imshow, xticks, yticks, show)
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn import model_selection
 
 from project_2 import *
 
+# Maximum number of KNN
+L = 30
+
+# Multiply the number of neigbors
+M = 2
+
 # K-nearest neighbors
-KNN = [e * 1 for e in range(1, 11)]
+KNN = [e * M for e in range(1, L + 1)]
 
 # Distance metric (corresponds to 2nd norm, euclidean distance).
 # You can set dist=1 to obtain manhattan distance (cityblock distance).
 dist = list(range(1, 3))
 
-# Outer K-fold crossvalidation
+# Two layer K-fold crossvalidation
 K_1 = 10
 K_2 = 10
 OCV = model_selection.KFold(n_splits=K_1, shuffle=True)
 ICV = model_selection.KFold(n_splits=K_2, shuffle=True)
 
-error_val = np.empty((10, 2, 10))
-error_gen = np.empty((10, 2))
+# Error matrixs
+error_val = np.empty((L, 2, 10))
+error_gen = np.empty((L, 2))
 errors = np.zeros(K_1)
 
+# Start two layer crossvalidation
 k_1 = 0
 for par_index, test_index in OCV.split(X_k):
     print('Outer Crossvalidation fold: {0}/{1}'.format(k_1 + 1, K_1))
@@ -43,42 +52,60 @@ for par_index, test_index in OCV.split(X_k):
         X_val = X_par[val_index, :]
         y_val = y_par[val_index]
 
-        # Train and test data
+        # Train and test data for every model
         for knn in KNN:
             for d in dist:
                 knclassifier = KNeighborsClassifier(n_neighbors=knn, p=d)
                 knclassifier.fit(X_train, y_train)
                 y_est = knclassifier.predict(X_val)
 
-                error_val[int(knn / 1 - 1)][(d - 1)][k_2] = np.sum(y_est != y_val, dtype=float) * len(X_val) / len(X_par)
+                # Calculate validation error * X_val / X_par
+                error_val[int(knn / M - 1)][(d - 1)][k_2] = np.sum(y_est != y_val, dtype=float) * len(X_val) / len(X_par)
 
         k_2 += 1
 
+    # Outer layer CV
+    # Calculate Generalisation Error for every model
     for knn in KNN:
         for d in dist:
-            error_gen[int(knn / 1 - 1)][d - 1] = np.sum(error_val[int(knn / 1 - 1)][d - 1], dtype=float)
+            error_gen[int(knn / M - 1)][d - 1] = np.sum(error_val[int(knn / M - 1)][d - 1], dtype=float)
 
-    print(error_gen)
-    # find model with min error
+    # find model with minimum error
     d_min_index = 0
     knn_min_index = 0
     min_error = 1000
     for knn in KNN:
-        min_temp_index = np.argmin(error_gen[int(knn / 1 - 1)])
+        min_temp_index = np.argmin(error_gen[int(knn / M - 1)])
 
-        if min_error > error_gen[int(knn / 1 - 1)][min_temp_index]:
-            min_error = error_gen[int(knn / 1 - 1)][min_temp_index]
+        if min_error > error_gen[int(knn / M - 1)][min_temp_index]:
+            min_error = error_gen[int(knn / M - 1)][min_temp_index]
             d_min_index = min_temp_index
             knn_min_index = knn
 
+    # Train on X_par
     knclassifier = KNeighborsClassifier(n_neighbors=knn_min_index, p=d_min_index + 1)
     knclassifier.fit(X_par, y_par)
     y_est = knclassifier.predict(X_test)
 
+    # Compute true generalisation error for the model * X_test / X_k
     errors[k_1] = np.sum(y_est != y_test, dtype=float) * len(X_test) / len(X_k)
+
+    # Plot Confusion Matrix
+    cm = confusion_matrix(y_test, y_est)
+    accuracy = 100 * cm.diagonal().sum() / cm.sum()
+    error_rate = 100 - accuracy
+    figure(2)
+    imshow(cm, cmap='binary', interpolation='None')
+    colorbar()
+    xticks(range(2))
+    yticks(range(2))
+    xlabel('Predicted class')
+    ylabel('Actual class')
+    title('Confusion matrix (Accuracy: {0}%, Error Rate: {1}%)'.format(accuracy, error_rate));
+    show()
 
     k_1 += 1
 
 
-# Plot the classification error rate
-print('Error rate: {0}%'.format(100 * np.sum(errors)))
+# Print the classification error rate
+print('Error rate: {0}%'.format(np.sum(errors)))
