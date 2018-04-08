116d734d0ec079c934682c19b951953b1eeb4c17
Martin Meincke <meincke91@hotmail.com>
2018-04-08 15:41:57 +0200

    Fixed errors

--- dtu_ml_data_mining/project_2/forward_selection.py
+++ dtu_ml_data_mining/project_2/forward_selection.py
@@ -1,9 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
-
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 from project_2 import *
+
+from toolbox.Tools.toolbox_02450 import feature_selector_lr, bmplot
+
+
 
 def forward_selection_cv(X, y, cv_split=5):
 
@@ -11,6 +17,8 @@ def forward_selection_cv(X, y, cv_split=5):
 
 	forward_selected_indexes = []
 	overall_highest_R2 = -1.0
+
+	errors = []
 
 	for j in range(X_train.shape[1]):
 		best_index = -1
@@ -23,7 +31,10 @@ def forward_selection_cv(X, y, cv_split=5):
 				model.fit(X_train[:,tmp_index],y_train)
 
 				score = model.score(X_test[:,tmp_index], y_test)
-				
+
+				if j == 0:
+					errors.append(mean_squared_error(y_test, model.predict(X_test[:,tmp_index])))
+
 				if highest_R2 < score:
 					highest_R2 = score
 					best_index = i
@@ -32,12 +43,16 @@ def forward_selection_cv(X, y, cv_split=5):
 			overall_highest_R2 = highest_R2
 			forward_selected_indexes.append(best_index)
 
+	plt.bar([i for i in range(len(errors))], errors)
+	plt.xticks(np.arange(len(errors)), (k_encoded_attr_names), rotation=45)
+	plt.show()
+
 	return forward_selected_indexes
+
 
 X_train, X_test, y_train, y_test = train_test_split(X_k[:,forward_selection_cv(X_k, grades[:, 2])], grades[:, 2], test_size=0.2)
 model = LinearRegression(normalize=True)
 model.fit(X_train,y_train)
-
-for i in range(20):
-	print("predicted:", model.predict(X_test[i]), " actual: ", y_test[i] )
+#print("predicted:", model.predict(X_test[i]), " actual: ", y_test[i] )
 print(model.score(X_test,y_test))
+print()
